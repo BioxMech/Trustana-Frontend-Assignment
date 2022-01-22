048a7ff1fb13c8278fa9bfbdd1a0d792d9e0a7d8
Jason <70564653+BioxMech@users.noreply.github.com>
2022-01-22 16:14:33 +0800

    Minor adjustment to the html codes to be more readable.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,118 +2,49 @@ import React from 'react';
 
 import TabGroup from "../tabs/tabGroup.component";
 import './header.css';
-
-import P1 from '../../assets/product1.svg';
-import P2 from '../../assets/product2.svg';
-import P3 from '../../assets/product3.svg';
+import TAB_DATA from './tab.data';
 
 function Header() {
 
   return (
     <div>
       <TabGroup>
-        {/* <CardGroup /> */}
-        <div label="Korean Products">
-          <div className="content">
-            <div className="content-catelog">Catelog</div>
-            <div className="content-title">Korean Products</div>
-            <div className="grid">
-              <div> 
-                <div className="card">
-                  <div className="card-image-background">
-                    <img src={P1} alt="P1" className="card-image" />
-                  </div>
-                  
-                  <div className="card-content">
-                    <div className="card-title">Product 1</div>
-                    <div className="card-details">
-                      <p>MDQ 2D Units</p>
-                      <p>SGD 4.50 /carton</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div> 
-                <div className="card">
-                  <div className="card-image-background">
-                    <img src={P2} alt="P2" className="card-image" />
-                  </div>
-                  
-                  <div className="card-content">
-                    <div className="card-title">Product 2</div>
-                    <div className="card-details">
-                      <p>MDQ 2D Units</p>
-                      <p>SGD 4.50 /carton</p>
-                    </div>
+        {
+          TAB_DATA.map((tab) => {
+            return (
+              <div label={tab.title}>
+                <div className="content">
+                  <div className="content-catelog">Catelog</div>
+                  <div className="content-title">Korean Products</div>
+                  <div className="grid">
+                      {
+                        tab.products.map(product => (
+                          <div>
+                          <div className="card">
+                            <div className="card-image-background">
+                              <img src={ product.image } alt={ product.image } className="card-image" />
+                            </div>
+                            
+                            <div className="card-content">
+                              <div className="card-title">{ product.productTitle }</div>
+                              <div className="card-details">
+                                <p>{ product.units }</p>
+                                <p>{ product.price }</p>
+                              </div>
+                            </div>
+                          </div>
+                          </div>
+                        ))
+                      }
                   </div>
                 </div>
               </div>
-              <div> 
-                <div className="card">
-                  <div className="card-image-background">
-                    <img src={P3} alt="P3" className="card-image" />
-                  </div>
-                  
-                  <div className="card-content">
-                    <div className="card-title">Product 3</div>
-                    <div className="card-details">
-                      <p>MDQ 2D Units</p>
-                      <p>SGD 4.50 /carton</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div label="Chinese Products">
-          <div className="content">
-            <div className="content-catelog">Catelog</div>
-            <div className="content-title">Chinese Products</div>
-            <div className="grid">
-              <div> 
-                <div className="card">
-                  <div className="card-image-background">
-                    <img src={P1} alt="P1" className="card-image" />
-                  </div>
-                  
-                  <div className="card-content">
-                    <div className="card-title">Product 1</div>
-                    <div className="card-details">
-                      <p>MDQ 2D Units</p>
-                      <p>SGD 4.50 /carton</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div label="Japanese Products">
-          <div className="content">
-            <div className="content-catelog">Catelog</div>
-            <div className="content-title">Japanese Products</div>
-            <div className="grid">
-              <div> 
-                <div className="card">
-                  <div className="card-image-background">
-                    <img src={P1} alt="P1" className="card-image" />
-                  </div>
-                  
-                  <div className="card-content">
-                    <div className="card-title">Product 1</div>
-                    <div className="card-details">
-                      <p>MDQ 2D Units</p>
-                      <p>SGD 4.50 /carton</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+            )
+          })
+        }
       </TabGroup>
     </div>
+    
   )
 }
 
---
+++ src/components/header/tab.data.js
@@ -0,0 +1,49 @@
+const TAB_DATA = [
+  {
+    'title': "Korean Products",
+    'products': [
+      {
+        'productTitle': 'Product 1',
+        'image': 'pictures/product1.svg',
+        'units': 'MDQ 2D Units',
+        'price': 'SGD 4.50 /carton'
+      },
+      {
+        'productTitle': 'Product 2',
+        'image': 'pictures/product2.svg',
+        'units': 'MDQ 2D Units',
+        'price': 'SGD 4.50 /carton'
+      },
+      {
+        'productTitle': 'Product 3',
+        'image': 'pictures/product3.svg',
+        'units': 'MDQ 2D Units',
+        'price': 'SGD 4.50 /carton'
+      }
+    ]
+  },
+  {
+    'title': "Chinese Products",
+    'products': [
+      {
+        'productTitle': 'Product 1',
+        'image': 'pictures/product1.svg',
+        'units': 'MDQ 2D Units',
+        'price': 'SGD 4.50 /carton'
+      }
+    ]
+  },
+  {
+    'title': "Japanese Products",
+    'products': [
+      {
+        'productTitle': 'Product 1',
+        'image': 'pictures/product1.svg',
+        'units': 'MDQ 2D Units',
+        'price': 'SGD 4.50 /carton'
+      }
+    ]
+  }
+]
+
+export default TAB_DATA;
